9436bd961188ad962a2079695d298a104ba17425
guillermoroman <guillermoroman@outlook.com>
2024-11-09 02:28:39 +0100

    Añadir guardado de personajes desde save_character.php

--- config/db.php
+++ config/db.php
@@ -12,7 +12,7 @@ try{
         $username,
         $password);
 
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);    
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);     
 
 } catch (PDOException $e){
     echo "Error de conexión: " . $e->getMessage();
--- save_character.php
+++ save_character.php
@@ -1,4 +1,21 @@
 <?php
-var_dump($_POST);
-echo "Nombre: " . $_POST["name"];
+require_once 'config/db.php';
+
+// var_dump($_POST);
+/*
+echo "Nombre: " . $_POST["name"] . "<br>";
 echo "Descripción: " . $_POST["description"];
+*/
+$name = $_POST["name"];
+$description = $_POST["description"];
+
+
+$stmt = $db->prepare("INSERT INTO characters (name, description) VALUES (:name, :description)");
+$stmt->bindParam(':name', $name);
+$stmt->bindParam(':description', $description);
+
+if ($stmt->execute()){
+    echo "Personaje creado";
+} else {
+    echo "Ha ocurrido un fallo";
+}
